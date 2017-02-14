a9526802f9b84c2e7c6287598877860d2a119931
Takeo Tamura <takeo@cubdesign.com>
2017-02-14 13:06:41 +0900

    add auto reload

--- es6/src/main.js
+++ es6/src/main.js
@@ -1,40 +1,42 @@
-$(document).ready(function(){	
-	
-	// Example 1
-	var dialOne = JogDial(document.getElementById('jog_dial_one'), 
-		{wheelSize:'200px', knobSize:'70px', minDegree:0, maxDegree:360, degreeStartAt: 0})
-		.on('mousemove', function(evt){
-			$('#jog_dial_one_meter div').css('width', Math.round((evt.target.rotation/360)*100) + '%' )
-		});
+import JogDial from './jogDial'
 
-	// Example 2
-	var dialTwo = JogDial(document.getElementById('jog_dial_two'), 
-		{debug:true, wheelSize:'260px', knobSize:'100px', degreeStartAt: 0})
-		.on('mousemove', function(evt){
-			$('#jog_dial_two_meter').text('Rotation:' + Math.round(evt.target.rotation) + ' / Degree: ' + Math.round(evt.target.degree));
-		});
+$(document).ready(function() {
 
-	$('.dial:nth-child(2)').css('opacity',0);
+  // Example 1
+  var dialOne = JogDial(document.getElementById('jog_dial_one'),
+    { wheelSize: '200px', knobSize: '70px', minDegree: 0, maxDegree: 360, degreeStartAt: 0 })
+    .on('mousemove', function(evt) {
+      $('#jog_dial_one_meter div').css('width', Math.round((evt.target.rotation / 360) * 100) + '%')
+    });
 
-	//Example swap buttons
-	$('.btn-group').children().on('click', function(){
-		var btn = this;
-		$(this).parent().children().each(function(i){
-			if(btn == this) {
-				$(this).addClass('active');
+  // Example 2
+  var dialTwo = JogDial(document.getElementById('jog_dial_two'),
+    { debug: true, wheelSize: '260px', knobSize: '100px', degreeStartAt: 0 })
+    .on('mousemove', function(evt) {
+      $('#jog_dial_two_meter').text('Rotation:' + Math.round(evt.target.rotation) + ' / Degree: ' + Math.round(evt.target.degree));
+    });
 
-				$('.dial').each(function(j){
-					if(i == j){
-						$(this).removeClass('hidden').css('opacity',1);
-					}
-					else{
-						$(this).addClass('hidden').css('opacity',0);
-					}
-				})
-			}
-			else {
-				$(this).removeClass('active');
-			}
-		})
-	});
+  $('.dial:nth-child(2)').css('opacity', 0);
+
+  //Example swap buttons
+  $('.btn-group').children().on('click', function() {
+    var btn = this;
+    $(this).parent().children().each(function(i) {
+      if (btn == this) {
+        $(this).addClass('active');
+
+        $('.dial').each(function(j) {
+          if (i == j) {
+            $(this).removeClass('hidden').css('opacity', 1);
+          }
+          else {
+            $(this).addClass('hidden').css('opacity', 0);
+          }
+        })
+      }
+      else {
+        $(this).removeClass('active');
+      }
+    })
+  });
 });
