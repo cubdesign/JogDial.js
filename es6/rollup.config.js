9518814d9bb4b9e30d176c1257314c58b3885e42
Takeo Tamura <takeo@cubdesign.com>
2017-02-14 14:34:43 +0900

    eslint setting

--- es6/rollup.config.js
+++ es6/rollup.config.js
@@ -6,10 +6,14 @@ import eslint from 'rollup-plugin-eslint';
 export default {
   entry: 'src/main.js',
   dest: 'dist/bundle.js',
+  format: 'iife',
+  sourceMap: 'inline',
   plugins: [
-    eslint(),
     nodeResolve({ jsnext: true }), // npmモジュールを`node_modules`から読み込む
     commonjs(), // CommonJSモジュールをES6に変換
-    babel() // ES5に変換
+    eslint(),
+    babel({
+      exclude: 'node_modules/**'
+    }) // ES5に変換
   ]
 }
