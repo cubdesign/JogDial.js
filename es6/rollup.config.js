5e132874bf87880cc049f26d00de0c6d8019e70e
Takeo Tamura <takeo@cubdesign.com>
2017-03-03 18:37:37 +0900

    change extension

--- es6/rollup.config.js
+++ es6/rollup.config.js
@@ -4,13 +4,17 @@ import commonjs     from 'rollup-plugin-commonjs'
 import eslint from 'rollup-plugin-eslint';
 import buble from 'rollup-plugin-buble'
 
+const options = {
+  ext: 'tag.html'
+}
+
 export default {
   entry: 'src/main.js',
   dest: 'dist/bundle.js',
   format: 'iife',
   // sourceMap: 'inline',
   plugins: [
-    riot(),
+    riot(options),
     nodeResolve({ jsnext: true }), // npmモジュールを`node_modules`から読み込む
     commonjs(), // CommonJSモジュールをES6に変換
     eslint(),
